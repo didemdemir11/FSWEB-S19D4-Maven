e251d093841c0fb9db332278c0e6be9ff9334e0a
didemdemir11 <kdidemdemir@gmail.com>
2025-01-23 14:11:44 +0300

    Complete S19D4 task dependencies

---
+++ src/main/java/com/workintech/s19d1/entity/Movie.java
@@ -0,0 +1,46 @@
+package com.workintech.s19d1.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "movie", schema = "fsweb")
+public class Movie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    @NotNull(message = "film adı null olamaz")
+    private String name;
+
+    @Column(name = "rating")
+    private Integer rating;
+
+    @Column(name = "release_date")
+    private LocalDate releaseDate;
+    @Column(name = "director_name")
+    private String directorName;
+
+    @ManyToMany(cascade = {CascadeType.DETACH,CascadeType.MERGE,CascadeType.PERSIST,CascadeType.REFRESH},fetch = FetchType.EAGER)
+    @JoinTable(name="movie_actor",schema = "fsweb",
+            inverseJoinColumns = @JoinColumn(name = "actor_id"),
+            joinColumns = @JoinColumn(name = "movie_id")
+            )
+    private List<Actor> actors;
+
+    public void addActor(Actor actor){
+        if(actors == null){
+            actors= new ArrayList<>();
+        }
+        actors.add(actor);
+    }
+}
